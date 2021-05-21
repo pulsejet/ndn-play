68c99c808730435699fc52500f0849b56f0eba6a
Varun Patil <radialapps@gmail.com>
2021-05-21 14:48:13 -0700

    Remove unused import

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-editor',

47c49d804258f52d0f671488190e942bc45b7517
Varun Patil <radialapps@gmail.com>
2021-10-29 16:58:11 -0700

    Expose keychain to user / add security template

--- src/app/user-types.ts
+++ src/app/user-types.ts
@@ -2,6 +2,7 @@ import { INode } from './interfaces';
 import * as packet from '@ndn/packet';
 import * as tlv from "@ndn/tlv";
 import * as sync from '@ndn/sync';
+import * as keychain from '@ndn/keychain';
 
 export const node: INode = <any>null;
-export const ndn = { packet, tlv, sync }
+export const ndn = { packet, tlv, sync, keychain }
